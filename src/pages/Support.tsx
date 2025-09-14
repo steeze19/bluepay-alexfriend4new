a6821ee0c28bf61aa6b602f090adbb5a70cfb3b8
steeze19 <steeze266@gmail.com>
2025-09-14 15:16:58 +0100

    Update Support.tsx

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -73,7 +73,7 @@ const Support = () => {
             </div>
             <Button 
               className="w-full mt-4 bg-blue-600 hover:bg-blue-700"
-              onClick={() => window.open('https://wa.me/2349082468216')}
+              onClick={() => window.open('https://wa.me/2348158591865')}
             >
               Chat on WhatsApp
             </Button>
