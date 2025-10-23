e3895fd9fa721ee451393bcd848e60805835ab4e
steeze19 <steeze266@gmail.com>
2025-10-23 14:37:55 +0100

    Update WhatsApp contact link in Support page

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -73,7 +73,7 @@ const Support = () => {
             </div>
             <Button 
               className="w-full mt-4 bg-blue-600 hover:bg-blue-700"
-              onClick={() => window.open('https://wa.me/2349152912665')}
+              onClick={() => window.open('https://wa.me/2347068756548')}
             >
               Chat on WhatsApp
             </Button>
